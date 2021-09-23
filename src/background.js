d91ef523b87bf7eb8aeed1c81b368c806f4fd464
Kaylie Gonzalez <kaylieag@tamu.edu>
2021-09-23 12:49:35 -0500

    youtube search from bang

--- src/background.js
+++ src/background.js
@@ -8,8 +8,37 @@ console.log(">>>", chrome.omnibox.onInputEntered.addListener);
 
 chrome.omnibox.onInputEntered.addListener(function(text){
     console.log(text);
-    NavigateTo("https://www.youtube.com"); //should select correct url down the line
+    const short = parseText(text);
+    if(short[0] == 'yt'){
+        if(short.length > 1){
+            short.splice(0,1);
+            let yt_url  = "https://www.youtube.com";
+            let temp = '';
+            let sq = short[0];
+            for(let i = 0; i < short.length - 1; i++){
+                temp = sq.concat('+', short[i+1]);
+                sq = temp;
+            }
+            let yt_sq = yt_url.concat('/results?search_query=',sq);
+            NavigateTo(yt_sq);
+        }
+        else{
+            NavigateTo("https://www.youtube.com");
+        }
+    }
+    else if(short[0] == 'hd'){
+        NavigateTo("https://howdy.tamu.edu");
+    }
+    else{
+        NavigateTo("https://canvas.tamu.edu");
+    }
+    //should select correct url down the line
 })
+
+function parseText(text){
+    const words = text.split(" ");
+    return words;
+}
 
 
 async function NavigateTo(url){
