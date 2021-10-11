4d2e7757828718f39098352a882fa597a2df97bc
Kaylie Gonzalez <kaylieag@tamu.edu>
2021-10-11 10:18:21 -0500

    youtube searchable

--- src/background.js
+++ src/background.js
@@ -57,3 +57,11 @@ async function  getCurrentTab(){
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
 }
+
+async function getCurrentUrl(){
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+        // use `url` here inside the callback because it's asynchronous!
+    });
+    return url;
+}
