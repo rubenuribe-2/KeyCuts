2cee5869ec1e2f03135957bc7d2e69bb945bd0e7
Ruben Uribe <rubenuribe@Rubens-MacBook-Pro.local>
2021-12-06 20:49:50 -0600

    add runtime args

--- tests/startup.js
+++ tests/startup.js
@@ -2,6 +2,11 @@
 const { assert } = require('console');
 const puppeteer = require('puppeteer');
 
+
+const args = process.argv.slice(2);
+
+console.log(args);
+
 function delay(time) {
     return new Promise(function(resolve) { 
         setTimeout(resolve, time)
@@ -57,11 +62,40 @@ async function createShortcuts(page,no_shortcuts){
     //gets the extension ID of the target
     const [,,extensionID,] = extensionTarget._targetInfo.url.split('/');
     const settings = await browser.newPage(); 
-    setTimeout(async ()=>{
+    await delay(300);
+    if(args[0]==='stress'){
         await settings.goto('chrome-extension://'+extensionID+'/settings.html');
         createShortcuts(settings,51);
+    }   if(args[0]==='popup') {
+        //test responsiveness of make KeyCut button
+        await settings.goto('chrome-extension://'+extensionID+'/popup.html');
+        const button = await settings.$('#keycut-btn');
+        //test what if blank:
+        const shortcut = await settings.$('#KeyCut-field');
+        const inputValue = await settings.$eval('#KeyCut-field', el => el.value);
+        console.log(inputValue);
+        for (let i = 0; i < inputValue.length; i++) {
+            await shortcut.press('Backspace');
+        }
+        var button_content = await settings.$eval('#keycut-btn', el => el.innerHTML);
+        console.assert(button_content === ':(', 'BLANK KC FIELD IN POPUP IS BEING ACCEPTED');
+        
+        
+        // test what if override
+        await shortcut.type('a');
+        button_content = await settings.$eval('#keycut-btn', el => el.innerHTML);
+        console.assert(button_content === "Replace 'a' KeyCut", 'a is not saying replaced IS BEING ACCEPTED');
 
-    },300);
+
+
+
+        
+        const url = await settings.$('#url-field');
+        // await shortcut.type('a');
+        // console.log(shortcut);
+    }
+
+
     // await browser.close();
     console.log("test completed")
 })();
